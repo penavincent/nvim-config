452f6edebb0a40de6a59e36a78fdacfd709787b5
Vincent Pena <joempena26@gmail.com>
2026-03-01 10:56:28 -0800

    Change shell to fish, modify theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
