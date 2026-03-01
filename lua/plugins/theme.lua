0694dd217c784052db18d862d88dd535021a0461
Vincent Pena <joempena26@gmail.com>
2026-03-01 13:55:15 -0800

    Add omarchy auto-themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,29 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      flavour = "auto",
-      background = {
-        light = "latte",
-        dark = "macchiato",
-      },
-      transparent_background = true,
-      float = {
-        transparent = true,
-        solid = true,
-      },
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.special.bufferline").get_theme()
-          end
-        end,
-      },
-    },
   },
   {
     "LazyVim/LazyVim",
