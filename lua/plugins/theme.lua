20b6ad06e7bf31061765185e10e546a3dff92b18
Vincent Pena <joempena26@gmail.com>
2026-04-04 13:45:20 -0500

    Fix themes and remove Omarchy setup

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,12 +2,37 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = true,
     priority = 1000,
+    opts = {
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true,
+      },
+      auto_integrations = true,
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.special.bufferline").get_theme()
+          end
+        end,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-nvim",
     },
   },
 }
