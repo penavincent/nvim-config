e3b2405d81ecaec6feda254c0048a6b3ce5f803e
Vincent Pena <joempena26@gmail.com>
2026-04-23 00:32:29 -0700

    Push only in roslyn

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and (client.name == "roslyn" or client.name == "roslyn_ls") then
+      client.server_capabilities.diagnosticProvider = nil
+    end
+  end,
+})
