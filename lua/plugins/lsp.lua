ffd583de4d04b02b27ea7912e7f48fd77030a086
Vincent Pena <joempena26@gmail.com>
2026-04-23 00:18:03 -0700

    Lock and lsp dynamic registration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,6 +6,15 @@ return {
       servers = {
         omnisharp = false,
         roslyn_ls = true,
+        ["*"] = {
+          capabilities = {
+            workspace = {
+              didChangeWatchedFiles = {
+                dynamicRegistration = true,
+              },
+            },
+          },
+        },
       },
     },
   },
