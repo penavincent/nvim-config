66b1e530d48821931e168bba1d9de0d4a4644f7b
Vincent Pena <joempena26@gmail.com>
2026-06-19 12:31:00 -0600

    Remove custom registry Mason

--- lua/plugins/mason-registries.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mason-org/mason.nvim",
-  opts = function(_, opts)
-    opts.registries = {
-      "github:mason-org/mason-registry",
-      "github:Crashdummyy/mason-registry",
-    }
-  end,
-}
