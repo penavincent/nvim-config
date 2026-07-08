757b294affb5768dc2c8a80ea0c24399c1da275d
Vincent Pena <penajoe@microsoft.com>
2026-07-08 23:16:03 +0000

    Update lock and add dotnet extra, disable omnisharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        omnisharp = false,
         roslyn_ls = {
           on_attach = function(client)
             client.server_capabilities.diagnosticProvider = false
